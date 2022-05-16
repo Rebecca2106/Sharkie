a21b1a0d52bad6db911301ffc83a1a2d360bd2ae
Rebecca2106 <you@example.com>
2022-05-16 12:27:36 +0200

    first_models

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -4,10 +4,20 @@ class MovableObject {
     img;
     height = 150;
     width = 150;
+    imageCache={};
+    currentImage=0;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(array){
+        array.forEach((path) => {
+            let img= new Image();
+            img.src=path;
+            this.imageCache[path]=img;
+        });
     }
 
     moveRight() {                        //function ist nicht nötig, OOP ist recht neu in JS, funktioniert ohne das Wort
